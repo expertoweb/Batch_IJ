d7863ef2f387cc8e60ec12c806e27fb95ac51a20
expertoweb <expertoweb@gmail.com>
2014-12-24 12:41:59 +0100

    Included in package es.expertoweb.Batch_IJ

---
+++ src/es/expertoweb/Batch_IJ/Batch_IJ.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.expertoweb.Batch_IJ;
+/**
+ *
+ * @author expertoweb
+ */
+public class Batch_IJ {
+    
+}
